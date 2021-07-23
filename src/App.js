7bd7f36c03652dbaf731adefde6fbf2daea2b1f4
이상민 <soone@isangmin-ui-MacBookPro.local>
2021-07-23 17:00:58 +0900

    대문변경 & 상품추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Navbar, Container, Nav, NavDropdown, Jumbotron, Button} from 'react-bootstrap';
+import data from './data.js'
 
 function App() {
+
+  let [shoes,shoes변경] = useState(data);
+  let products = [<Product1/>,<Product2/>,<Product3/>];
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -25,7 +30,7 @@ function App() {
         </Container>
       </Navbar>
 
-      <Jumbotron>
+      <Jumbotron className="background">
         <h1>20% Season Off</h1>
         <p>
           This is a simple hero unit, a simple jumbotron-style component for calling
@@ -35,8 +40,46 @@ function App() {
           <Button variant="primary">Learn more</Button>
         </p>
       </Jumbotron>
+      <div className="container"> 
+        <div className="row">
+          <Product1 상품명 = {shoes[0].title} 내용 = {shoes[0].content} 가격 = {shoes[0].price} />
+          <Product2 상품명 = {shoes[1].title} 내용 = {shoes[1].content} 가격 = {shoes[1].price} />
+          <Product3 상품명 = {shoes[2].title} 내용 = {shoes[2].content} 가격 = {shoes[2].price} />
+        </div>
+      </div>
     </div>
   );
 }
+
+function Product1(props) {
+  return(
+  <div className="col-md-4"><img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+    <h4>{props.상품명}</h4>
+    <p>{props.내용}</p>
+    <p>{props.가격}₩</p>
+  </div>
+  )
+}
+
+function Product2(props) {
+  return(
+  <div className="col-md-4"><img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>
+    <h4>{props.상품명}</h4>
+    <p>{props.내용}</p>
+    <p>{props.가격}₩</p>
+  </div>
+  )
+}
+
+function Product3(props) {
+  return(
+  <div className="col-md-4"><img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>
+    <h4>{props.상품명}</h4>
+    <p>{props.내용}</p>
+    <p>{props.가격}₩</p>
+  </div>
+  )
+}
+
 
 export default App;
